869f85ce83696c41982bd9501917aec1a4f76eaa
Stephan Preis <stephan.preis.wisconsin@gmail.com>
2017-06-14 21:34:49 +0200

    Die Crop-Werte von letzter Woche kommen jetzt mal ins Repository. Damit hat man in einem neu geklonten Repository bessere Anfangswerte.

---
+++ cropValues.tcl
@@ -0,0 +1,27 @@
+array set crop {
+  Donnerstag,W 130
+  Donnerstag,H 500
+  Donnerstag,x 150
+  Donnerstag,y 220
+  Freitag,f 2
+  Freitag,W 130
+  Freitag,H 500
+  Freitag,x 277
+  Freitag,y 220
+  Montag,f 0
+  Mittwoch,f 2
+  Montag,W 149
+  Dienstag,f 0
+  Montag,x 300
+  Montag,H 300
+  Dienstag,W 150
+  Mittwoch,W 145
+  Montag,y 208
+  Dienstag,x 444
+  Dienstag,H 300
+  Mittwoch,x 1
+  Mittwoch,H 500
+  Dienstag,y 220
+  Mittwoch,y 150
+  Donnerstag,f 2
+}
